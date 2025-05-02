b8a270e11f95a7fe7da3266130dddb8d20d96e76
santosh.k <santosh.k@apollodart.com>
2025-05-02 15:50:20 +0530

    Landing page is implemented

---
+++ components/app/nav-bar.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="navbar bg-primary text-primary-content">
+    <div class="container mx-auto">
+      <div class="navbar-start">
+        <NuxtLink to="/" class="btn btn-ghost text-xl">
+          Travel logs
+        </NuxtLink>
+      </div>
+
+      <div class="navbar-end">
+        <a class="btn btn-accent">Sign in with Github
+          <Icon name="tabler:brand-github" size="20" />
+        </a>
+      </div>
+    </div>
+  </div>
+</template>
